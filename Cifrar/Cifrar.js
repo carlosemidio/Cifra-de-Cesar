3c729a3e0fcae07003e38f9f16126ceec9bc1870
Carlos Emídio <carlosemidiopereira@gmail.com>
2018-08-14 11:16:19 -0300

    Correções

--- Cifrar/Cifrar.js
+++ Cifrar/Cifrar.js
@@ -8,9 +8,17 @@ function cifrar(msg, chave) {
     for (var i = 0; i < words.length; i++) {
         for (var j = 0; j < words[i].length; j++) {
             if(words[i][j].charCodeAt(0) >= 65 && words[i][j].charCodeAt(0) <= 90) {
-                msgCifrada += alfaBeto[alfaBeto.indexOf(words[i][j])+(parseInt(chave)%26)];
+                if (alfaBeto.indexOf(words[i][j])+(parseInt(chave)%26) <= 25) {
+                    msgCifrada += alfaBeto[alfaBeto.indexOf(words[i][j])+(parseInt(chave)%26)];
+                } else {
+                    msgCifrada += alfaBeto[(alfaBeto.indexOf(words[i][j])+(parseInt(chave)%26))-26];
+                }
             } else if (words[i][j].charCodeAt(0) >= 97 && words[i][j].charCodeAt(0) <= 122) {
-                msgCifrada += alfaBeto[alfaBeto.indexOf(words[i][j].toUpperCase())+(parseInt(chave)%26)].toLowerCase();
+                if ((alfaBeto.indexOf(words[i][j].toUpperCase())+(parseInt(chave)%26)) <= 25) {
+                    msgCifrada += alfaBeto[alfaBeto.indexOf(words[i][j].toUpperCase())+(parseInt(chave)%26)].toLowerCase();    
+                } else {
+                    msgCifrada += alfaBeto[(alfaBeto.indexOf(words[i][j].toUpperCase())+(parseInt(chave)%26))-26].toLowerCase();
+                }
             } else {
                 msgCifrada += words[i][j];
             }
--- Decifrar/Descifrar.js
+++ Decifrar/Descifrar.js
@@ -2,24 +2,35 @@ var alfaBeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function descifrar(msg, chave) {
     var words = msg.split(" ");
-    var msgCifrada = "";
+    var msgDescifrada = "";
     
     // Desfaz a troca de letras
     for (var i = 0; i < words.length; i++) {
         for (var j = 0; j < words[i].length; j++) {
-            if(words[i][j].charCodeAt(0) >= 65 && words[i][j].charCodeAt(0) <= 90) {
-                msgCifrada += alfaBeto[alfaBeto.indexOf(words[i][j])+(parseInt(chave)%26)];
-            } else if (words[i][j].charCodeAt(0) >= 97 && words[i][j].charCodeAt(0) <= 122) {
-                msgCifrada += alfaBeto[alfaBeto.indexOf(words[i][j].toUpperCase())-(parseInt(chave)%26)].toLowerCase();
+            if(alfaBeto.indexOf(words[i][j].toUpperCase()) >= 0) {
+                if((alfaBeto.indexOf(words[i][j].toUpperCase()) - parseInt(chave)) >= 0) {
+                    if (alfaBeto.indexOf(words[i][j]) >= 0) {
+                        msgDescifrada += alfaBeto[alfaBeto.indexOf(words[i][j].toUpperCase())-parseInt(chave)];
+                    } else {
+                        msgDescifrada += alfaBeto[alfaBeto.indexOf(words[i][j].toUpperCase())-parseInt(chave)].toLowerCase();
+                    }
+
+                } else {
+                    if (alfaBeto.indexOf(words[i][j]) >= 0) {
+                        msgDescifrada += alfaBeto[26 +(alfaBeto.indexOf(words[i][j].toUpperCase())-parseInt(chave))];    
+                    }else {
+                        msgDescifrada += alfaBeto[26 +(alfaBeto.indexOf(words[i][j].toUpperCase())-parseInt(chave))].toLowerCase();
+                    }
+                }
             } else {
-                msgCifrada += words[i][j];
+                msgDescifrada += words[i][j];
             }
         }
 
         // Separa as palavras com espaço
-        msgCifrada += " ";
+        msgDescifrada += " ";
     }
 
     // Retorna a mensagem descifrada
-    return msgCifrada;
+    return msgDescifrada;
 }
