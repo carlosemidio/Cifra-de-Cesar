b5850b0dd7cf2068d9a844a77ce34e84a33a6874
Carlos Emídio <carlosemidiopereira@gmail.com>
2018-08-14 08:16:24 -0300

    Descifrar

--- Decifrar/Descifrar.js
+++ Decifrar/Descifrar.js
@@ -1,16 +1,16 @@
 var alfaBeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-function cifrar(msg, chave) {
+function descifrar(msg, chave) {
     var words = msg.split(" ");
     var msgCifrada = "";
     
-    // Faz a troca das letras
+    // Desfaz a troca de letras
     for (var i = 0; i < words.length; i++) {
         for (var j = 0; j < words[i].length; j++) {
             if(words[i][j].charCodeAt(0) >= 65 && words[i][j].charCodeAt(0) <= 90) {
                 msgCifrada += alfaBeto[alfaBeto.indexOf(words[i][j])+(parseInt(chave)%26)];
             } else if (words[i][j].charCodeAt(0) >= 97 && words[i][j].charCodeAt(0) <= 122) {
-                msgCifrada += alfaBeto[alfaBeto.indexOf(words[i][j].toUpperCase())+(parseInt(chave)%26)].toLowerCase();
+                msgCifrada += alfaBeto[alfaBeto.indexOf(words[i][j].toUpperCase())-(parseInt(chave)%26)].toLowerCase();
             } else {
                 msgCifrada += words[i][j];
             }
@@ -20,6 +20,6 @@ function cifrar(msg, chave) {
         msgCifrada += " ";
     }
 
-    // Retorna a mensagem cifrada
+    // Retorna a mensagem descifrada
     return msgCifrada;
 }
